44de2d1ca28455132e9985c2840676f13367fba7
Sebastian <sebastian.pfeiffer@hs-furtwangen.de>
2021-10-18 22:54:41 +0200

    bad agent movement

--- LaserLeague/Script/Source/Main.ts
+++ LaserLeague/Script/Source/Main.ts
@@ -1,19 +1,49 @@
 namespace Script {
   import ƒ = FudgeCore;
-  ƒ.Debug.info("Main Program Template running!")
+  ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
-
+  let transform: ƒ.Matrix4x4;
+  let transform1: ƒ.Matrix4x4;
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    let graph: ƒ.Node = viewport.getBranch();
+    let laserbase: ƒ.Node = graph.getChildrenByName("LaserStruckture")[0].getChildrenByName("LaserBase")[0];
+    let agent: ƒ.Node = graph.getChildrenByName("Agent")[0];
+    transform = laserbase.getComponent(ƒ.ComponentTransform).mtxLocal;
+    transform1 = agent.getComponent(ƒ.ComponentTransform).mtxLocal;
   }
+  document.addEventListener("keydown", hndKeypress);
+  async function hndKeypress(_event: KeyboardEvent): Promise<void> {
+    switch (_event.code) {
+      case ƒ.KEYBOARD_CODE.W:
+        transform1.translateZ(-0.5)
+        break;
+      case ƒ.KEYBOARD_CODE.S: 
+      transform1.translateZ(0.5)       
+        break;
+      case ƒ.KEYBOARD_CODE.A:
+        transform1.translateX(-0.5)
+        break;
+      case ƒ.KEYBOARD_CODE.D:
+        transform1.translateX(0.5)
+        break;
+    }
 
+  }
   function update(_event: Event): void {
     // ƒ.Physics.world.simulate();  // if physics is included and used
+    
+    
+    //let lasercomp = laserbase.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(90);
+    
+    //console.log("output: " + graph);
+    
+    transform.rotateY(5);
     viewport.draw();
     ƒ.AudioManager.default.update();
   }

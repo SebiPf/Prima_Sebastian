f2310e7c1b18fa172ca31d4224c6d1f9b73c8d98
Sebastian <sebastian.pfeiffer@hs-furtwangen.de>
2021-10-26 01:19:46 +0200

    collision

--- LaserLeague/Script/Build/Script.js
+++ LaserLeague/Script/Build/Script.js
@@ -36,59 +36,92 @@ var Script;
 (function (Script) {
     var ƒ = FudgeCore;
     ƒ.Debug.info("Main Program Template running!");
+    //let speedagent: number = 1;
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
+    //let rotation: ƒ.Matrix4x4;
+    let rotatingspeed = 360;
     let transform;
-    let transform1;
+    let agent;
+    let laserbase;
+    let forward = new ƒ.Control("Forward", 10, 0 /* PROPORTIONAL */);
+    forward.setDelay(300);
+    let agenttransform;
     function start(_event) {
         viewport = _event.detail;
-        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
         let graph = viewport.getBranch();
-        let laserbase = graph.getChildrenByName("LaserStruckture")[0].getChildrenByName("LaserBase")[0];
-        let agent = graph.getChildrenByName("Agent")[0];
+        laserbase = graph.getChildrenByName("LaserStruckture")[0].getChildrenByName("LaserBase")[0];
+        agent = graph.getChildrenByName("Agent")[0];
         transform = laserbase.getComponent(ƒ.ComponentTransform).mtxLocal;
-        transform1 = agent.getComponent(ƒ.ComponentTransform).mtxLocal;
+        //rotation = graph.getChildrenByName("LaserBeam")[0].mtxLocal;
+        agenttransform = agent.getComponent(ƒ.ComponentTransform).mtxLocal;
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, 60); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
-    document.addEventListener("keydown", hndKeypress);
-    async function hndKeypress(_event) {
-        switch (_event.code) {
-            case ƒ.KEYBOARD_CODE.W:
-                transform1.translateZ(-0.5);
-                break;
-            case ƒ.KEYBOARD_CODE.S:
-                transform1.translateZ(0.5);
-                break;
-            case ƒ.KEYBOARD_CODE.A:
-                transform1.translateX(-0.5);
-                break;
-            case ƒ.KEYBOARD_CODE.D:
-                transform1.translateX(0.5);
-                break;
-        }
-    }
+    /* document.addEventListener("keydown", hndKeypress);
+    async function hndKeypress(_event: KeyboardEvent): Promise<void> {
+      switch (_event.code) {
+        case ƒ.KEYBOARD_CODE.W:
+          transform1.translateZ(-0.1)
+          break;
+        case ƒ.KEYBOARD_CODE.S:
+        transform1.translateZ(0.1)
+          break;
+        case ƒ.KEYBOARD_CODE.A:
+          transform1.translateX(-0.1)
+          break;
+        case ƒ.KEYBOARD_CODE.D:
+          transform1.translateX(0.1)
+          break;
+      }
+  
+    } */
     function update(_event) {
-        let x = transform1.translation.x;
-        let z = transform1.translation.z;
-        //console.log("testttttt " + x)
-        if (x > 9) {
-            transform1.translateX(-18.5);
+        let deltatime = ƒ.Loop.timeFrameReal / 1000;
+        let turnagent = 360;
+        transform.rotateY(rotatingspeed * deltatime);
+        let value = (ƒ.Keyboard.mapToValue(-1, 0, [ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN])
+            + ƒ.Keyboard.mapToValue(1, 0, [ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP]));
+        forward.setInput(value * deltatime);
+        agent.mtxLocal.translateX(forward.getOutput());
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT]) == true) {
+            agenttransform.rotateY(turnagent * deltatime);
         }
-        if (x < -9) {
-            transform1.translateX(18.5);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]) == true) {
+            agenttransform.rotateY(-turnagent * deltatime);
         }
-        if (z > 6.5) {
-            transform1.translateZ(-13.5);
-        }
-        if (z < -6.5) {
-            transform1.translateZ(13.5);
-        }
+        /* let x = agenttransform.translation.x;
+   
+       let z = agenttransform.translation.z;
+       //console.log("testttttt " + x)
+       if(x > 9){
+         agenttransform.translateX(-18.5)
+       }
+       if(x < -9){
+         agenttransform.translateX(18.5)
+       }
+       if(z > 6.5){
+         agenttransform.translateZ(-13.5)
+       }
+       if(z < -6.5){
+         agenttransform.translateZ(13.5)
+       }  */
         // ƒ.Physics.world.simulate();  // if physics is included and used
-        //let lasercomp = laserbase.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(90);
         //console.log("output: " + graph);
-        transform.rotateY(5);
+        checkCollision();
         viewport.draw();
         ƒ.AudioManager.default.update();
+    }
+    function checkCollision() {
+        let beam = laserbase.getChildren()[0];
+        let posLocal = ƒ.Vector3.TRANSFORMATION(agent.mtxWorld.translation, beam.mtxWorldInverse, true);
+        //console.log("check: " + posLocal.toString());
+        console.log(agenttransform.translation.x);
+        if (posLocal.x > -5 && posLocal.x < 0 && posLocal.z < 0.5 && posLocal.z > -0.5) {
+            console.log("Collision ");
+            agenttransform.translation.x = 5;
+            agent.mtxWorld.translation.x = 5;
+        }
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
